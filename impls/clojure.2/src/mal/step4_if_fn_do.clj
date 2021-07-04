fb34948d3b603774b8fcb721ff0e82a3fd19e1f8
Søren Sjørup <soren@zoren.dk>
2021-07-05 00:20:22 +0200

    step 4 do and if

---
+++ impls/clojure.2/src/mal/step4_if_fn_do.clj
@@ -0,0 +1,104 @@
+(ns mal.step4-if-fn-do
+  (:require
+   [mal.printer]
+   [mal.reader]
+   [mal.env]))
+
+(defn READ [s]
+  (mal.reader/read-form s))
+
+(def EVAL)
+
+(defn eval-ast [ast env]
+  (cond
+    (symbol? ast)
+    (mal.env/get-symbol ast env)
+
+    (list? ast)
+    (map #(EVAL % env) ast)
+
+    (vector? ast)
+    (mapv #(EVAL % env) ast)
+
+    (map? ast)
+    (into {} (for [[k v] ast] [k (EVAL v env)]))
+
+    :else
+    ast))
+
+(defn EVAL [ast env]
+  (if (list? ast)
+    (if (empty? ast)
+      ast
+      (let [[first-form & forms] ast]
+        (cond
+          (= first-form 'def!)
+          (let [[name exp] forms
+                evaluated-exp (EVAL exp env)]
+            (mal.env/set-symbol name evaluated-exp env)
+            evaluated-exp)
+
+          (= first-form 'let*)
+          (let [[bindings body] forms
+                new-env (mal.env/make-env env)]
+            (doseq [[name exp] (partition 2 bindings)]
+              (mal.env/set-symbol name (EVAL exp new-env) new-env))
+            (EVAL body new-env))
+
+          (= first-form 'do)
+          (do
+            (doseq [form (butlast forms)]
+              (EVAL form env))
+            (EVAL (last forms) env))
+
+          (= first-form 'if)
+          (let [[condition true-form false-form] forms]
+            (if (EVAL condition env)
+              (EVAL true-form env)
+              (EVAL false-form env)))
+
+          :else
+          (apply (EVAL first-form env) (for [form forms] (EVAL form env))))))
+    (eval-ast ast env)))
+
+(defn PRINT [s]
+  (mal.printer/pr-str s))
+
+(defn rep [input env]
+  (->
+   input
+   READ
+   (EVAL env)
+   PRINT))
+
+(def start-env
+  {'+ (fn [x y] (+ x y))
+   '- (fn [x y] (- x y))
+   '* (fn [x y] (* x y))
+   '/ (fn [x y] (quot x y))
+   'prn (fn [form] (println (pr-str form)) nil)
+   'list list
+   'list? list?
+   'empty? empty?
+   'count count
+   '= =
+   '< <
+   '<= <=
+   '> >
+   '>= >=})
+
+(def repl-env
+  (mal.env/make-env))
+
+(doseq [[s f] start-env]
+  (mal.env/set-symbol s f repl-env))
+
+(loop []
+  (print "user> ")
+  (flush)
+  (when-let [line (read-line)]
+    (try
+      (println (rep line repl-env))
+      (catch Exception e
+        (println (str "error:" (ex-message e)))))
+    (recur)))
