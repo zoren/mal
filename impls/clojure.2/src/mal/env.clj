c8c163a91a345ecee3f72fd64eddc30943bda0b9
Søren Sjørup <soren@zoren.dk>
2021-07-05 22:52:39 +0200

    variadic function parameters

--- impls/clojure.2/src/mal/env.clj
+++ impls/clojure.2/src/mal/env.clj
@@ -1,13 +1,26 @@
 (ns mal.env)
 
-(defn make-env 
+(defn bind [params args]
+  (let [before-amp (take-while (fn [s] (not= s '&)) params)
+        [_ rest] (drop-while (fn [s] (not= s '&)) params)
+        binding-map (into {} (map vector before-amp (take (count before-amp) args)))]
+    (if rest
+      (assoc binding-map rest (into () (drop (count before-amp) args)))
+      binding-map)))
+
+(comment
+  (bind '(& r) '(1 2 3))
+  (bind '(x & r) '(1 2 3))
+  (bind '(x y & r) '(1 2 3)))
+
+(defn make-env
   ([] (make-env nil {}))
   ([outer] (make-env outer {}))
   ([outer initial-bindings]
    {:outer outer
     :data (atom initial-bindings)})
   ([outer binds values]
-  (make-env outer (into {} (map vector binds values)))))
+   (make-env outer (bind binds values))))
 
 (defn set-symbol [k v {data :data}]
   (swap! data assoc k v)
@@ -18,7 +31,7 @@
     (if (contains? @data k)
       current-env
       (when outer (recur outer)))))
- 
+
 (defn get-symbol [k env]
   (if-let [{data :data} (find-symbol k env)]
     (@data k)
