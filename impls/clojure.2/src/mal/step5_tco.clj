6f02fa16edd63340810760beaac1c48d4c906d4d
Søren Sjørup <soren@zoren.dk>
2021-07-31 23:42:21 +0200

    implement step 5

---
+++ impls/clojure.2/src/mal/step5_tco.clj
@@ -0,0 +1,106 @@
+(ns mal.step4-if-fn-do
+  (:require
+   [mal.printer]
+   [mal.reader]
+   [mal.env]
+   [mal.core]))
+
+(defn READ [s]
+  (mal.reader/read-form s))
+
+(def EVAL)
+
+(defn eval-ast [ast env]
+  (cond
+    (symbol? ast)
+    (mal.env/get-symbol ast env)
+
+    (list? ast)
+    (map #(EVAL % env) ast)
+
+    (vector? ast)
+    (mapv #(EVAL % env) ast)
+
+    (map? ast)
+    (into {} (for [[k v] ast] [k (EVAL v env)]))
+
+    :else
+    ast))
+
+(defn mal-apply [f a]
+  (if (fn? f)
+    (f a)))
+
+(defn EVAL [ast env]
+  (if (list? ast)
+    (if (empty? ast)
+      ast
+      (let [[first-form & forms] ast]
+        (cond
+          (= first-form 'def!)
+          (let [[name exp] forms
+                evaluated-exp (EVAL exp env)]
+            (mal.env/set-symbol name evaluated-exp env)
+            evaluated-exp)
+
+          (= first-form 'let*)
+          (let [[bindings body] forms
+                new-env (mal.env/make-env env)]
+            (doseq [[name exp] (partition 2 bindings)]
+              (mal.env/set-symbol name (EVAL exp new-env) new-env))
+            (recur body new-env))
+
+          (= first-form 'do)
+          (let [el (eval-ast (butlast forms) env)]
+            (recur (last el) env))
+
+          (= first-form 'if)
+          (let [[condition true-form false-form] forms]
+            (if (EVAL condition env)
+              (recur true-form env)
+              (recur false-form env)))
+
+          (= first-form 'fn*)
+          (let [[params body] forms]
+            {:ast body
+             :params params
+             :env env})
+
+          :else
+          (let [f (EVAL first-form env)
+                args (for [form forms] (EVAL form env))]
+            (if (fn? f)
+              (apply f args)
+
+              (let [{:keys [ast params env]} f
+                    new-env (mal.env/make-env env params args)]
+                (recur ast new-env)))))))
+    (eval-ast ast env)))
+
+(defn PRINT [s]
+  (mal.printer/pr-str s))
+
+(defn rep [input env]
+  (->
+   input
+   READ
+   (EVAL env)
+   PRINT))
+
+(def repl-env
+  (mal.env/make-env))
+
+(doseq [[s f] mal.core/ns]
+  (mal.env/set-symbol s f repl-env))
+
+(rep "(def! not (fn* (a) (if a false true)))" repl-env)
+
+(loop []
+  (print "user> ")
+  (flush)
+  (when-let [line (read-line)]
+    (try
+      (println (rep line repl-env))
+      (catch Exception e
+        (println (str "error:" (ex-message e)))))
+    (recur)))
